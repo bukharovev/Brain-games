959313b26e41cd3f1232dfc11157d17d23524b82
bukharovev <bukharovev@gmail.com>
2019-03-02 18:21:52 +0300

    add prime

---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+import toPlay from '..';
+import random from '../utils';
+
+const isPrime = (num) => {
+  if (num === 1) {
+    return false;
+  }
+  for (let i = 2; i < Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const task = 'Answer "yes" if number is prime otherwise answer "no"';
+export default () => {
+  const gameSettings = () => {
+    const num = random(1, 100);
+    const question = num;
+    const rightAnswer = (isPrime(num) ? 'yes' : 'no');
+    return { question, rightAnswer };
+  };
+  toPlay(task, gameSettings);
+};
